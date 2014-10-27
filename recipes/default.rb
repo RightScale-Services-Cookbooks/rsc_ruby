9360fc741bf4e28a9cdcadec8678238634fd65cd
Curt Wilhelm <curt@rightscale.com>
2014-10-27 12:52:49 -0600

    install bundler and rake as part of ruby

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,7 +9,9 @@ log "installing Ruby #{node[:rsc_ruby][:ruby][:version]} to #{node[:rsc_ruby][:r
 ruby_build_ruby node[:rsc_ruby][:ruby][:version] do
   prefix_path node[:rsc_ruby][:ruby][:prefix_path]
 end
+
 ENV["PATH"]="#{node[:rsc_ruby][:ruby][:prefix_path]}/bin:#{ENV["PATH"]}"
+log "PATH #{ENV["PATH"]}"
 
 log "install rubygems"
 execute "#{node[:rsc_ruby][:ruby][:prefix_path]}/bin/gem update --system"
@@ -18,7 +20,13 @@ log "install rubygems from gem_package"
 gem_package "rubygems-update" do
   gem_binary "#{node[:rsc_ruby][:ruby][:prefix_path]}/bin/gem"
 end
+gems= ["bundler","rake"]
 
+gems.each do |gem|
+  gem_package gem do
+    gem_binary "#{node[:rsc_ruby][:ruby][:prefix_path]}/bin/gem"
+  end
+end
 
 ohai "reload" do
   action :reload
